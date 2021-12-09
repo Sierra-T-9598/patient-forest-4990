4782d2c8bfe703fd3c1fc2f463a31df6d13e0338
Sierra Tucker <sierra.tucker121@gmail.com>
2021-12-09 10:37:37 -0700

    Create model test for sorting actors ::by_age

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -5,4 +5,20 @@ require 'rails_helper'
       it {should have_many :actor_movies}
       it {should have_many(:movies).through(:actor_movies)}
     end
+
+    describe 'class methods' do
+      describe '::by_age' do
+        it 'lists actors from youngest to oldest' do
+          universal = Studio.create!(name: 'Universal Studios', location: 'Hollywood')
+          raiders = universal.movies.create!(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure')
+          shrek = universal.movies.create!(title: 'Shrek', creation_year: 2001, genre: 'Comedy')
+          actor_1 = raiders.actors.create!(name: 'Harrison Ford', age: 65)
+          actor_2 = raiders.actors.create!(name: 'Paul Freeman', age: 68)
+          actor_3 = raiders.actors.create!(name: 'Karen Allen', age: 50)
+          actor_4 = shrek.actors.create!(name: 'Eddie Murphy', age: 40)
+
+          expect(Actor.by_age).to eq([actor_4, actor_3, actor_1, actor_2])
+        end
+      end
+    end
   end
