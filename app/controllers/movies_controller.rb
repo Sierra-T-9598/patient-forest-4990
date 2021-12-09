a4e77f572ab17337707207a71f9746b781484881
Sierra Tucker <sierra.tucker121@gmail.com>
2021-12-09 11:29:05 -0700

    Delete ActorMovies Controller, change create route/form in movie show view

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,20 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
+
+  def new
+    @movie = Movie.find(params[:id])
+  end
+
+  def create
+    @movie = Movie.find(params[:id])
+    @actor = @movie.actors.create!(actor_params)
+    @actor.save
+    # redirect_to "/movies/#{@move.id}"
+  end
+
+  private
+    def actor_params
+      params.permit(:name, :age)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/studios', to: 'studios#index'
 
   get '/movies/:id', to:'movies#show'
-
-  get '/movies/:id/actors/new', to: 'actor_movies#new'
-  post '/movies/:id', to: 'actor_movies#create'
+  # get '/movies/:id/new', to: 'movies#new'
+  post '/movies/:id', to: 'movies#create'
+  # post '/movies/:id', to: 'actor_movies#create'
 end
